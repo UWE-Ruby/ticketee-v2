6b812b3319094a1bbac2c6170411fc986bee182a
Ivan Storck <ivanoats@gmail.com>
2013-05-23 17:30:38 -0700

    heroku fix for asset init on precompile

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Ticketee
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # heroku
+    config.assets.initialize_on_precompile = false
   end
 end
