25cd4ee8f4e27b95ac54f3edfff6c732fc47ec7f
Ivan Storck <ivanoats@gmail.com>
2013-08-05 10:06:14 -0700

    fix duplicate ticket titles and descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(:version => 20120519013012) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
-    t.string   "uncomfirmed_email"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
